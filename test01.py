45cad26bd08de1c34cae5c83822932f5ddaba9bd
Kevin McAleer <kevinmcaleer@gmail.com>
2022-09-17 15:49:50 +0100

    Added web UI for arm

--- test01.py
+++ test01.py
@@ -4,14 +4,16 @@ import math
 
 board = Inventor2040W()
 
-base = board.servos[SERVO_6]
-base.enable()
-elbow = board.servos[SERVO_5]
-elbow.enable()
+for servo in board.servos:
+    servo.to_mid()
+    sleep(1)
 
-base.to_mid()
-elbow.to_min()
-sleep(1)
+    
+# base = board.servos[SERVO_6]
+# base.enable()
+# elbow = board.servos[SERVO_5]
+# elbow.enable()
+
 
 SWEEPS = 3              # How many sweeps of the servo to perform
 STEPS = 10              # The number of discrete sweep steps
@@ -21,5 +23,6 @@ SWEEP_EXTENT = 90.0     # How far from zero to move the servo when sweeping
 # Do a sine sweep
 for j in range(SWEEPS):
     for i in range(360):
-        base.value(math.sin(math.radians(i)) * SWEEP_EXTENT)
-        sleep(0.02)
+        for servo in board.servos:
+            servo.value(math.sin(math.radians(i)) * SWEEP_EXTENT)
+            sleep(0.02)
---
+++ test02.py
@@ -0,0 +1,69 @@
+from phew import *
+from phew import connect_to_wifi, server, logging
+from phew import render_template
+from config import wifi_ssid, wifi_password
+from inventor import Inventor2040W, SERVO_1, SERVO_2, SERVO_3, SERVO_4, SERVO_5, SERVO_6 
+from time import sleep
+import math
+
+
+# Connect to WiFi
+ip = connect_to_wifi(wifi_ssid, wifi_password)
+
+# Create a new board
+board = Inventor2040W()
+
+# Set all servos to mid position
+for servo in board.servos:
+    servo.to_mid()
+    print(f'servo: {servo}, value {servo.value()}')
+    sleep(1)
+
+def position(arm=None, wrist=None, elbow=None, finger=None, base=None):
+    if finger is not None:
+        board.servos[SERVO_2].value(finger)
+    if arm is not None:
+        board.servos[SERVO_4].value(arm)
+    if wrist is not None:
+        board.servos[SERVO_3].value(wrist)
+    if elbow is not None:
+        board.servos[SERVO_5].value(elbow)
+    if base is not None:
+        board.servos[SERVO_6].value(base)
+    sleep(0.01)
+
+@server.route('/', methods=['GET','POST'])
+def index(request):
+#     print(f'request: {request.method}, form data: {request.form}')
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        
+        elbow = request.form.get("elbow", None)
+        arm = request.form.get("arm", None)
+        base = request.form.get("base", None)
+        finger = request.form.get("finger", None)
+        wrist = request.form.get("wrist", None)
+        if elbow is not None:
+            position(elbow=int(elbow))
+        if arm is not None:
+            position(arm=int(arm))
+        if base is not None:
+            position(base=int(base))
+        if wrist is not None:
+            position(wrist=int(wrist))
+        if finger is not None:
+            position(finger=int(finger))
+        # elbow = request.form['elbow']
+        # arm = request.form['arm']
+        # base = request.form['base']
+        # finger = request.form['finger']
+        # wrist = request.form['wrist']
+#         logging.info('elbow: %s, arm: %s, base: %s, finger: %s, wrist: %s' % (elbow, arm, base, finger, wrist))
+
+    return render_template('index.html')
+    
+logging.info(f'IP: {ip}')
+logging.is_disabled = True
+
+server.run()
